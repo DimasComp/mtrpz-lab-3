f2755884ea09fc018038086a785c90a2ddb995ba
Demchyk Dmytro <d.demchyk.im21@kpi.ua>
2024-06-09 19:58:45 +0300

    some changes to project

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Just run <code>docker build -t mtrpz-lab-3 .</code> 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <b>Some bold text....</b>
       </header>
     </div>
   );
